073e5fff1bc37e6776b6fe80d5bee4b47c3e05ab
zhiyuan <zhiyuan_zheng@roboticplus.com>
2025-09-25 00:16:54 +0800

    添加一个可以操作引擎后台的节点

---
+++ StateMachine/BaseNodes/EngineBackendNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace StateMachine
+{
+    [DebuggerNonUserCode]
+    public abstract class EngineBackendNode : BaseGroupNode
+    {
+        public FSMEngine? Backend => Engine;
+
+    }
+}
