cb7e0367bddf3815359c20b609af2bcc49768ac7
zhiyuan <zhiyuan_zheng@roboticplus.com>
2025-12-18 13:31:26 +0800

    添加一个脚本解析帮助类

---
+++ StateMachine/ScriptAnalyzer/ConnectLineListener.cs
@@ -0,0 +1,69 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+
+namespace StateMachine.ScriptAnalyzer
+{
+    public static class StateMachineScriptAnalyzer
+    {
+        public static void ReadScript(this string input,
+            Action<IVisualNode> nodeCreateAction,
+            Action<string, string, string> connectAction)
+        {
+            var stream = new AntlrInputStream(input);
+            var lexer = new StateMachineScriptLexer(stream);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new StateMachineScriptParser(tokens);
+            var tree = parser.machine();
+
+            var nodeListener = new NodeListener();
+            var connectListener = new ConnectLineListener();
+
+            try
+            {
+                nodeListener.StateCreated += nodeCreateAction;
+                connectListener.LineConnected += connectAction;
+                var walker = new Antlr4.Runtime.Tree.ParseTreeWalker();
+                walker.Walk(nodeListener, tree);
+                walker.Walk(connectListener, tree);
+            }
+            finally
+            {
+                nodeListener.StateCreated -= nodeCreateAction;
+                connectListener.LineConnected -= connectAction;
+            }
+
+        }
+
+    }
+
+    internal class ConnectLineListener : StateMachineScriptBaseListener
+    {
+        public event Action<string, string, string> LineConnected;
+
+        private string nameprev;
+
+        public override void EnterNamespace([NotNull] StateMachineScriptParser.NamespaceContext context)
+        {
+            nameprev = context.STRING().ToString() + ".";
+            base.EnterNamespace(context);
+        }
+
+        public override void ExitNamespace([NotNull] StateMachineScriptParser.NamespaceContext context)
+        {
+            nameprev = "";
+            base.ExitNamespace(context);
+        }
+
+        public override void ExitDefTransition([NotNull] StateMachineScriptParser.DefTransitionContext context)
+        {
+            var event_name = nameprev + context.STRING()[0].GetText();
+            var sourceState_name = nameprev + context.STRING()[1].GetText();
+            var targetState_name = nameprev + context.STRING()[2].GetText();
+
+            LineConnected?.Invoke(event_name, sourceState_name, targetState_name);
+
+            base.ExitDefTransition(context);
+        }
+
+    }
+}
---
+++ StateMachine/ScriptAnalyzer/VisualNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateMachine.ScriptAnalyzer
+{
+    internal class VisualNode : IVisualNode
+    {
+
+        //脚本中的命名空间
+        public string NamePrefix { get; set; }
+
+        //当前节点对象在脚本内的名称
+        public string Name { get; set; }
+
+        public double PosX { get; set; }
+
+        public double PosY { get; set; }
+
+        public string FlowID { get; set; }
+
+        public string ClassType { get; set; }
+
+        public string Color { get; set; }
+
+        public string Discription { get; set; }
+
+        public List<NodeEventDescription> EventDescriptions { get; set; } = [];
+
+        public void UpdateEventDescriptions()
+        {
+        }
+    }
+}
